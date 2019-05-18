4eef9d8d205336c3d1ee37bee89f6579381c283b
ictrobot <ictrobot@outlook.com>
2019-05-18 14:44:49 +0100

    Make all buttons the same width

--- src/main/java/uk/ac/cam/cl/interactiondesign/group10/frontend/components/WButton.java
+++ src/main/java/uk/ac/cam/cl/interactiondesign/group10/frontend/components/WButton.java
@@ -5,13 +5,14 @@ import javafx.scene.control.Button;
 /**
  * WButton = Weather App Button
  * Customizes default button class for our weather app, avoids having to set the same properties repeatedly
- * Ensures the button uses the correct font
+ * Ensures the button uses the correct font and that all buttons are the same size
  */
 public class WButton extends Button {
 
   public WButton(String text) {
     super(text);
     setFont(WFont.MAIN);
+    setMinWidth(200);
   }
 
 }
